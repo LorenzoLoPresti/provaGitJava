6a1645499ee7da1e3ef6e844edc4dac707fa88e3
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-04-27 15:24:28 +0200

    prova

--- spring_jpa_progettoW5/src/main/java/com/jpa_project/runner/AppRunner.java
+++ spring_jpa_progettoW5/src/main/java/com/jpa_project/runner/AppRunner.java
@@ -37,98 +37,98 @@ public class AppRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Run...");
-
+		
 		// EDIFICIO
 		
-		// creo edificio nel db
-		// edificioService.creaEdificioNelDb();
-		
-		// leggo edificio dal db
-		Edificio edificioLetto = edificioService.cercaEdificioPerId(1l);
-		Edificio edificioLetto2 = edificioService.cercaEdificioPerId(2l);
-		
-        System.out.println(edificioLetto);
-
-        // update edificio nel db
-        edificioLetto.setCitta("Roma");
-        edificioService.updateEdificio(edificioLetto);
-        
-        // elimino edificio dal db
-        // edificioService.eliminaEdificio(edificioLetto);
-  
-
-		// POSTAZIONI
-        
-        // creo postazione nel db
-        // postazioneService.creaPostazione("Grande sala per grande conferenza", edificioLetto, Tipo.OPENSPACE);
-
-        // leggo postazione dal db
-		Postazione postazioneLetta = postazioneService.cercaPostazionePerId(1l);
-		System.out.println(postazioneLetta);
-		
-		// cerco postazione per tipo e città
-//		List<Postazione> listaPostazione = postazioneService.ricercaPostazione_tipo_città(Tipo.OPENSPACE, "Roma");
-//		listaPostazione.forEach(e -> System.out.println(e));
-		
-		// cerco postazione per edificio
-		List<Postazione> listaPostazionePerEd = postazioneService.cercaPostazionePerEdificio(edificioLetto);
-
-		// modifico Postazione
-//		postazioneLetta.setEdificio(edificioLetto2);
-//		postazioneService.updatePostazione(postazioneLetta);
-		
-		// elimino postazione
-		//postazioneService.rimuoviPostazione(postazioneLetta);
-		
-		// UTENTI
-		// crea utente nel db
-		// utenteService.creaUtente();
-
-		// leggi utente dal db
-		Utente utenteLetto = utenteService.cercaUtentePerId(1l);
-		System.out.println(utenteLetto);
-		
-		// trova tutti gli utenti
-		// List<Utente> listaUtenti = utenteService.cercaTuttiGliUtenti();
-		// utenteService.cercaTuttiGliUtenti().forEach(e -> System.out.println(e));
-
-		// modifica utente
-		utenteLetto.setNome("Andrea");
-		utenteService.updateUtente(utenteLetto);
-		System.out.println(utenteLetto);
-		
-		// elimina utente
-		utenteService.rimuoviUtentePerId(2l);
-		
-		
-		// PRENOTAZIONI
-		
-		// crea prenotazione
-//		prenotazioneService.creaPrenotazione(postazioneLetta, LocalDate.now(), utenteLetto);
-		
-		// leggi prenotazione
-		Prenotazione prenotazione1 = prenotazioneService.cercaPrenotazioneById(1l);
-		System.out.println(prenotazione1);
-		
-		// modifica prenotazione
-//		prenotazione1.setDataPrenotazione(LocalDate.now().plusMonths(1));
-//		prenotazioneService.updatePrenotazione(prenotazione1);
+//		// creo edificio nel db
+//		// edificioService.creaEdificioNelDb();
+//		
+//		// leggo edificio dal db
+//		Edificio edificioLetto = edificioService.cercaEdificioPerId(1l);
+//		Edificio edificioLetto2 = edificioService.cercaEdificioPerId(2l);
+//		
+//        System.out.println(edificioLetto);
+//
+//        // update edificio nel db
+//        edificioLetto.setCitta("Roma");
+//        edificioService.updateEdificio(edificioLetto);
+//        
+//        // elimino edificio dal db
+//        // edificioService.eliminaEdificio(edificioLetto);
+//  
+//
+//		// POSTAZIONI
+//        
+//        // creo postazione nel db
+//        // postazioneService.creaPostazione("Grande sala per grande conferenza", edificioLetto, Tipo.OPENSPACE);
+//
+//        // leggo postazione dal db
+//		Postazione postazioneLetta = postazioneService.cercaPostazionePerId(1l);
+//		System.out.println(postazioneLetta);
+//		
+//		// cerco postazione per tipo e città
+////		List<Postazione> listaPostazione = postazioneService.ricercaPostazione_tipo_città(Tipo.OPENSPACE, "Roma");
+////		listaPostazione.forEach(e -> System.out.println(e));
+//		
+//		// cerco postazione per edificio
+//		List<Postazione> listaPostazionePerEd = postazioneService.cercaPostazionePerEdificio(edificioLetto);
+//
+//		// modifico Postazione
+////		postazioneLetta.setEdificio(edificioLetto2);
+////		postazioneService.updatePostazione(postazioneLetta);
+//		
+//		// elimino postazione
+//		//postazioneService.rimuoviPostazione(postazioneLetta);
+//		
+//		// UTENTI
+//		// crea utente nel db
+//		// utenteService.creaUtente();
+//
+//		// leggi utente dal db
+//		Utente utenteLetto = utenteService.cercaUtentePerId(1l);
+//		System.out.println(utenteLetto);
+//		
+//		// trova tutti gli utenti
+//		// List<Utente> listaUtenti = utenteService.cercaTuttiGliUtenti();
+//		// utenteService.cercaTuttiGliUtenti().forEach(e -> System.out.println(e));
+//
+//		// modifica utente
+//		utenteLetto.setNome("Andrea");
+//		utenteService.updateUtente(utenteLetto);
+//		System.out.println(utenteLetto);
+//		
+//		// elimina utente
+//		utenteService.rimuoviUtentePerId(2l);
+//		
+//		
+//		// PRENOTAZIONI
+//		
+//		// crea prenotazione
+////		prenotazioneService.creaPrenotazione(postazioneLetta, LocalDate.now(), utenteLetto);
+//		
+//		// leggi prenotazione
+//		Prenotazione prenotazione1 = prenotazioneService.cercaPrenotazioneById(1l);
 //		System.out.println(prenotazione1);
-		
-		// elimina prenotazione
-//		prenotazioneService.rimuoviPrenotazionePerId(3l);
-		
-		// cerca tutte le prenotazioni
-//		List<Prenotazione> listaPrenotazioni = prenotazioneService.cercaTuttePrenotazioni();
-//		listaPrenotazioni.forEach(e -> System.out.println(e));
-		
-		// cerca postazione per edificio
-//		List<Postazione> listEdifici = postazioneService.cercaPostazionePerEdificio(edificioLetto);
-//		System.out.println(listEdifici.size());
-
-		// cerca numero prenotazioni per una deteminata postazione in una determinata data
-		// System.out.println(prenotazioneService.cercaPostazionePerData(prenotazione1.getDataPrenotazione(), postazioneLetta));	
-		
+//		
+//		// modifica prenotazione
+////		prenotazione1.setDataPrenotazione(LocalDate.now().plusMonths(1));
+////		prenotazioneService.updatePrenotazione(prenotazione1);
+////		System.out.println(prenotazione1);
+//		
+//		// elimina prenotazione
+////		prenotazioneService.rimuoviPrenotazionePerId(3l);
+//		
+//		// cerca tutte le prenotazioni
+////		List<Prenotazione> listaPrenotazioni = prenotazioneService.cercaTuttePrenotazioni();
+////		listaPrenotazioni.forEach(e -> System.out.println(e));
+//		
+//		// cerca postazione per edificio
+////		List<Postazione> listEdifici = postazioneService.cercaPostazionePerEdificio(edificioLetto);
+////		System.out.println(listEdifici.size());
+//
+//		// cerca numero prenotazioni per una deteminata postazione in una determinata data
+//		// System.out.println(prenotazioneService.cercaPostazionePerData(prenotazione1.getDataPrenotazione(), postazioneLetta));	
+//		
 	}
 
 }
